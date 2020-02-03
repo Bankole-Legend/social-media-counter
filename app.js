0b2b59ff13cd2d4a954fe9baa56ef2fd298b9394
Pius Reis <pius.reis777@gmail.com>
2020-02-03 16:03:34 +0100

    Initail Commit

---
+++ app.js
@@ -0,0 +1,25 @@
+/**
+ * SocialMedia Word Counter App
+ * The App is to count how many words the user has typed based on the 
+ * Social Media handle they have selected. And when they reach the limit
+ * of the SM handle they receive a message You have reached your maximum word count.
+ * 
+ * Bellow are the word counts for each social media
+ * Twitter = 280
+ * Facebook = 63206
+ * Instagram = 2200
+ * Others = 8000. 
+ * 
+ * When a user selects a SocialMedia Handle, Words in the html page
+ * changes from 0 to the desired number of words. E.g if twitter is selected, word changes form 0 280.centered
+ * 
+ * When a user begins to type in the textarea, Word Count increases while word decreases.
+ * 
+ * Once word decreases to 0. The user is unable to type any further characters and gets the
+ * congratulatory message. 
+ * 
+ * NOTE
+ * 1. Your code must be properly commented.
+ * 2. Use Es6 syntax wherever possible.  
+ */
+
